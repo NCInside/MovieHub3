bf0283d3f18222f236c67fc0de38d738d05ddf39
Nicholas Christian Irawan <113526916+NCInside@users.noreply.github.com>
2023-06-03 15:57:38 +0700

    responsive movie listing

--- MovieHub3/Views/Movie/MovieCard.swift
+++ MovieHub3/Views/Movie/MovieCard.swift
@@ -10,29 +10,42 @@ import SwiftUI
 struct MovieCard: View {
     
     var movie: Movie
+    var size: CGFloat
+    private let isIOS = {
+        #if os(iOS)
+        return true
+        #else
+        return false
+        #endif
+    }
     
     var body: some View {
         VStack(alignment: .leading) {
             movie.image
                 .resizable()
-                .frame(minHeight: 270.0, maxHeight: 270.0)
+                .scaledToFill()
+//                .frame(minHeight: 270.0, maxHeight: 270.0)
+                .frame(width: size * (isIOS() ? 0.45 : 0.23))
+                .clipped()
             HStack(spacing: 3) {
                 ForEach (0..<(Int(movie.score)+1), id: \.self) {_ in
                     Image(systemName: "star.fill")
+                        .font(.system(size: size * (isIOS() ? 0.035 : 0.0175)))
                         .foregroundColor(Color(red: 255/255.0, green: 192/255.0, blue: 69/255.0))
                 }
                 ForEach (0..<Int(5-movie.score), id: \.self) {_ in
                     Image(systemName: "star")
+                        .font(.system(size: size * (isIOS() ? 0.035 : 0.0175)))
                         .foregroundColor(Color(red: 255/255.0, green: 192/255.0, blue: 69/255.0))
                 }
             }
             .padding(.top, 10)
             Text(movie.title)
-                .font(.system(size: 18, weight: .heavy, design: .default))
+                .font(.system(size: size * (isIOS() ? 0.04 : 0.02), weight: .heavy, design: .default))
                 .foregroundColor(.white)
                 .padding(.top, 5)
             Text("\(movie.genres[0]) | \(movie.duration / 60)h \(movie.duration % 60)m | \(movie.rating)")
-                .font(.system(size: 14, weight: .medium, design: .default))
+                .font(.system(size: size * (isIOS() ? 0.031 : 0.0155), weight: .medium, design: .default))
                 .foregroundColor(.gray)
                 .padding(.top, 1)
         }
--- MovieHub3/Views/Movie/MovieList.swift
+++ MovieHub3/Views/Movie/MovieList.swift
@@ -11,9 +11,7 @@ struct MovieList: View {
     
     @StateObject private var viewModel = MovieViewModel()
     
-    private let columns = [
-        GridItem(.adaptive(minimum: 150))
-    ]
+    private let columns : [GridItem]
     
     init() {
         #if os(iOS)
@@ -22,42 +20,55 @@ struct MovieList: View {
         searchBarAppearance.tintColor = .red
         let textFieldAppearance = UISearchTextField.appearance()
         textFieldAppearance.backgroundColor = .white
+        
+        columns = Array(repeating: .init(.flexible()), count: 2)
+        
+        #else
+        columns = Array(repeating: .init(.flexible()), count: 4)
+        
         #endif
     }
     
     var body: some View {
-        NavigationStack {
-            ScrollView {
-                Picker("Category", selection: $viewModel.selectedCategory) {
-                    ForEach(MovieViewModel.MovieCategory.allCases) {category in
-                        Text(category.rawValue).tag(category)
-                    }
-                }
-                .pickerStyle(SegmentedPickerStyle())
-                .padding(.horizontal, 10)
-                LazyVGrid(columns: columns) {
-                    ForEach(viewModel.movies) {movie in
-                        NavigationLink {
-                            MovieDetail(movie: movie)
-                        } label: {
-                            MovieCard(movie: movie)
+        GeometryReader { geo in
+            NavigationStack {
+                ScrollView {
+                    Picker("Category", selection: $viewModel.selectedCategory) {
+                        ForEach(MovieViewModel.MovieCategory.allCases) {category in
+                            Text(category.rawValue).tag(category)
                         }
-                        .tag(movie)
-                        .buttonStyle(PlainButtonStyle())
                     }
+                    .pickerStyle(SegmentedPickerStyle())
+                    .padding(.horizontal, 10)
+                    .labelsHidden()
+                    #if !os(iOS)
+                    .padding(.vertical, 10)
+                    #endif
+                    LazyVGrid(columns: columns) {
+                        ForEach(viewModel.movies) {movie in
+                            NavigationLink {
+                                MovieDetail(movie: movie)
+                            } label: {
+                                MovieCard(movie: movie, size: geo.size.width)
+                            }
+                            .tag(movie)
+                            .buttonStyle(PlainButtonStyle())
+                        }
+                    }
+                    .padding(.horizontal, 10)
+                    #if !os(iOS)
+                    .searchable(text: $viewModel.searchText, placement: .toolbar)
+                    #endif
+
                 }
-                .padding(.horizontal, 10)
-                #if !os(iOS)
-                .searchable(text: $viewModel.searchText, placement: .toolbar)
-                #endif
+                .navigationTitle("Movies")
+                .background(Color.black)
             }
-            .navigationTitle("Movies")
-            .background(Color.black)
-        }
-        #if os(iOS)
-        .searchable(text: $viewModel.searchText, placement: .navigationBarDrawer(displayMode: .always), prompt: "Search for Movies")
-        #endif
+            #if os(iOS)
+            .searchable(text: $viewModel.searchText, placement: .navigationBarDrawer(displayMode: .always), prompt: "Search for Movies")
+            #endif
         .accentColor(Color(red: 217/255.0, green: 37/255.0, blue: 29/255.0))
+        }
     }
 }
 
